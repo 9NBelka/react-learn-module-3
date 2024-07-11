279cfec35a2b9699b67a994c7454f1473d64c2d8
Nikita <belka.nik02@gmail.com>
2024-07-11 20:00:41 +0300

    Вывод в консоль информацию, которую ввел пользователь + select

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ function App() {
 
     const handleUsername = (e) => {
       e.preventDefault();
-      console.log(e.target.elements.username.value)
+      console.log(e.target.elements.username.value);
+      console.log(e.target.elements.role.value);
+
     }
   return (
     <>
       <form onSubmit={handleUsername}>
         <input type="text" name="username"></input>
+        <select name="role">
+          <option value="guest">guest</option>
+          <option value="admin">admin</option>
+          <option value="user">user</option>
+
+        </select>
         <button type="submit">Submit</button>
       </form>
     </>
