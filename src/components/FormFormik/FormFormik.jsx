f92e6f6b7dc302b68505b63bba193bd3eff1c6ed
Nikita <belka.nik02@gmail.com>
2024-07-13 18:00:39 +0300

    Библиотека Yup. Валидация

--- src/components/FormFormik/FormFormik.jsx
+++ src/components/FormFormik/FormFormik.jsx
@@ -1,7 +1,12 @@
-import { Formik, Form, Field} from "formik"
+import { Formik, Form, Field, ErrorMessage} from "formik"
 import { useId } from "react"
+import * as Yup from 'yup'
 
-  
+  const UserSchema = Yup.object().shape({
+    username: Yup.string().min(3, 'Too short!').max(20, 'Too Long!').required('This is required you dummy!'),
+    email: Yup.string().email('Must be in email format').required('This is required you dummy!'),
+    role: Yup.string().oneOf(["guest","user", "admin"]).required('This is required you dummy!')
+  })
 
 const FormFormik = ({onDoHandleSubmit}) => {
 
@@ -18,17 +23,20 @@ const FormFormik = ({onDoHandleSubmit}) => {
       // console.log(value);
       onDoHandleSubmit(value);
       actions.resetForm();
-      }}>
+      }}
+      validationSchema={UserSchema}>
       <Form>
         <div>
           <label htmlFor={usernameFieldId}>Name:</label>
           <Field name="username" id={usernameFieldId} />
+          <ErrorMessage name="username" component="span"/>
         </div>
         <div>
           <label htmlFor={emailFieldId}>Email:</label>
           <Field type="email" name="email" id={emailFieldId} />
+          <ErrorMessage name="email" component="span"/>
         </div>
-        <Field as="textarea"/>
+        {/* <Field as="textarea"/> */}
         <div>
           <label htmlFor={roleFieldId}>Role:</label>
           <Field as="select" name="role" id={roleFieldId}>
@@ -36,6 +44,7 @@ const FormFormik = ({onDoHandleSubmit}) => {
             <option value="admin">admin</option>
             <option value="user">user</option>
           </Field>
+          <ErrorMessage name="role" component="span"/>
         </div>
         <button type="submit">submit</button>
       </Form>
